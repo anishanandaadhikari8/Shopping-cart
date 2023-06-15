08bf11497d8b6cf1451488e944d0fbeac1bcb104
Anish <anishanandaadhikari8@gmail.com>
2023-06-15 02:59:21 -0700

    pushing Shopping cart from local

---
+++ ecommerce-shopping-cart-master/src/components/Copyright.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Copyright extends Component {
+    render() {
+        return (
+            <div>
+                <h4>IMPORTANT LINKS</h4>
+                <ul>
+                    <li>
+                        <a href="https://www.weather.com">Weather</a>
+                    </li>
+                    <li>
+                        <a href="https://www.exampleblog.com">Blog</a>
+                    </li>
+                    <li>
+                        <a href="https://www.maps.com">Maps</a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
